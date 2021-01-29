8fb8ffb6b4ece11125c0d2547d3f260eb9ce8206
he-can <947318277@qq.com>
2021-01-30 02:16:40 +0800

    Site updated: 2021-01-30 02:16:36

---
+++ js/script.js
@@ -0,0 +1,25 @@
+import("./jquery.min.js")
+
+// aplayer延迟三秒播放
+setTimeout(function() {
+    $("div.aplayer-button.aplayer-play").click();
+}, 3000);
+
+
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/images/favicon7.png");
+        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/images/favicon5.png");
+        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
